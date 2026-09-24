33bb56c922318171229b2df0442072616143cc8f
Dilawar Ali <dilawarnaeem45@gmail.com>
2026-09-25 00:45:25 +0500

    fix(vercel): add ignore command for mobile changes and update build config

---
+++ react-native.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+  dependencies: {
+    'react-native-gesture-handler': {
+      platforms: {
+        android: null,
+        ios: null,
+      },
+    },
+    'react-native-reanimated': {
+      platforms: {
+        android: null,
+        ios: null,
+      },
+    },
+    'react-native-worklets': {
+      platforms: {
+        android: null,
+        ios: null,
+      },
+    },
+  },
+};
